9b4a541f1ab1aa7cbe9f89efb5b7a3f06765a856
Hanna Steine <hanna.steine@nmbu.no>
2024-03-11 09:33:30 +0100

    Starting to get there

--- plot.py
+++ plot.py
@@ -14,21 +14,26 @@ def read_benchmark_results(algorithm_name):
 def new_figure(height=55):
     return plt.figure(figsize=(84/25.4, height/25.4))
 
-# Generating and saving a plot
-def generate_plot(x, y, title, xlabel, ylabel, file_name):
-    fig = new_figure()
-    ax = fig.add_subplot(1, 1, 1)
-    ax.errorbar(x, y['mean'], yerr=y['std'], fmt='o-', label=title)
+def generate_line_plot(df, title, xlabel, ylabel, file_name):
+    fig, ax = plt.subplots(figsize=(10, 6))
+
+    # Plot the 'size' against the specified columns
+    df.plot(x='size', y=['sorted', 'resversed', 'random'], ax=ax, marker='o', linestyle='-')
+
+    ax.set_title(title)
     ax.set_xlabel(xlabel)
     ax.set_ylabel(ylabel)
-    ax.legend()
-    fig.savefig(file_name, bbox_inches='tight')
+    ax.legend(['Sorted', 'Reversed', 'Random'])
+
+    plt.savefig(file_name, bbox_inches='tight')
+    plt.show()
+
 
 if __name__ == "__main__":
     # Combining the benchmark results from different algorithms
     merge_sort_df = read_benchmark_results('mergeSort')
     quicksort_df = read_benchmark_results('quicksort')
-    #insertion_sort_df = read_benchmark_results('insertion_sort')
+    # insertion_sort_df = read_benchmark_results('insertion_sort')
 
     print(merge_sort_df.columns)
     print(quicksort_df.columns)
@@ -36,17 +41,10 @@ if __name__ == "__main__":
     # Assuming all dataframes have the same 'size' column
     x_values = merge_sort_df['size']
 
-    # Generating and save plot for mergeSort
-    generate_plot(x_values, merge_sort_df[['random', 'sorted', 'reversed']],
-                  title='Merge Sort', xlabel='Input Size', ylabel='Running Time',
-                  file_name='merge_sort_plot.pdf')
+    # Generating and save line plot for Merge Sort
+    generate_line_plot(merge_sort_df, title='Merge Sort', xlabel='Input Size', ylabel='Running Time',
+                       file_name='merge_sort_line_plot.pdf')
 
-    # Generating and save plot for quicksort
-    generate_plot(x_values, quicksort_df[['random', 'sorted', 'reversed']],
-                  title='Quicksort', xlabel='Input Size', ylabel='Running Time',
-                  file_name='quicksort_plot.pdf')
-
-    # Generating and save plot for insertion sort
-    #generate_plot(x_values, insertion_sort_df[['random', 'sorted', 'reversed']],
-                  #title='Insertion Sort', xlabel='Input Size', ylabel='Running Time',
-                  #file_name='insertion_sort_plot.pdf')
+    # Generating and save line plot for Quicksort
+    generate_line_plot(quicksort_df, title='Quicksort', xlabel='Input Size', ylabel='Running Time',
+                       file_name='quicksort_line_plot.pdf')
