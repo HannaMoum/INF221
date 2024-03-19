66af9abd5e7f1dabf241689f590f6338a35a024b
Hanna Steine <hanna.steine@nmbu.no>
2024-03-19 13:13:58 +0100

    plots

--- plot.py
+++ plot.py
@@ -13,28 +13,28 @@ def read_benchmark_results(algorithm_name):
     df = pd.read_pickle(file_path)
 
     if algorithm_name == "quicksort":
-        df = df.assign(theoretical_avg = (df['size'] * np.log2(df['size'])) / 100000)
-        df = df.assign(theoretical_bst = (df['size'] * np.log2(df['size'])) / 100000)
-        df = df.assign(theoretical_wrs = (df['size']^2) / 100000)
+        df = df.assign(theoretical_avg = (df['size'] * np.log2(df['size'])) / 10000000)
+        df = df.assign(theoretical_bst = (df['size'] * np.log2(df['size'])) / 10000000)
+        df = df.assign(theoretical_wrs = (df['size']**2) / 10000000)
 
     elif algorithm_name == "insertionSort":
-        df = df.assign(theoretical_avg = (df['size']^2) / 1000)
-        df = df.assign(theoretical_bst=(df['size'] * np.log2(df['size'])) / 1000)
-        df = df.assign(theoretical_wrs=(df['size'] ^ 2) / 1000)
+        df = df.assign(theoretical_avg = (df['size']**2) / 10000000)
+        df = df.assign(theoretical_bst= (df['size']) / 10000000)
+        df = df.assign(theoretical_wrs= (df['size']**2) / 10000000)
     else:
-        df = df.assign(theoretical_avg = (df['size'] * np.log2(df['size'])) / 1000000)
-        df = df.assign(theoretical_bst=(df['size'] * np.log2(df['size'])) / 1000000)
-        df = df.assign(theoretical_wrs=(df['size'] ^ 2) / 1000000)
+        df = df.assign(theoretical_avg=(df['size'] * np.log2(df['size'])) / 10000000)
+        df = df.assign(theoretical_bst=(df['size'] * np.log2(df['size'])) / 10000000)
+        df = df.assign(theoretical_wrs=(df['size'] * np.log2(df['size'])) / 10000000)
 
     return df
 
 
-# Creating a new figure of defined size
-def new_figure(height=55):
-    return plt.figure(figsize=(84/25.4, height/25.4))
+def new_figure(width=10, height=6):
+    return plt.figure(figsize=(width, height))
 
 
 def generate_plot(df, xlabel, ylabel, file_name):
+    plt.rcParams.update({'font.size': 18})
     fig, ax = plt.subplots(figsize=(10, 6))
 
     # Plot the 'size' against the specified columns without legend
@@ -44,20 +44,7 @@ def generate_plot(df, xlabel, ylabel, file_name):
     ax.set_xlabel(xlabel)
     ax.set_ylabel(ylabel)
 
-    plt.savefig(file_name, bbox_inches='tight')
-    plt.show()
-
-
-def generate_random_plot(algorithms, algorithm_dfs, xlabel, ylabel, file_name):
-    fig, ax = plt.subplots(figsize=(10, 6))
-
-    # Plot the 'size' against the 'random' column for each algorithm without legend
-    for algo, df in zip(algorithms, algorithm_dfs):
-        ax.plot(df['size'], df['random'], marker='o', linestyle='-', label=algo)
-
-    ax.set_xlabel(xlabel)
-    ax.set_ylabel(ylabel)
-
+    plt.grid(True)
     plt.savefig(file_name, bbox_inches='tight')
     plt.show()
 
@@ -69,20 +56,14 @@ if __name__ == "__main__":
     insertion_sort_df = read_benchmark_results('insertionSort')
 
     # Generating and save line plot for Merge Sort
-    generate_plot(merge_sort_df, xlabel='Input Size', ylabel='Running Time',
+    generate_plot(merge_sort_df, xlabel='Input Size', ylabel='Running Time (sec)',
                   file_name='mergesort_line_plot.pdf')
 
     # Generating and save line plot for Quick Sort
-    generate_plot(quicksort_df, xlabel='Input Size', ylabel='Running Time',
+    generate_plot(quicksort_df, xlabel='Input Size', ylabel='Running Time (sec)',
                   file_name='quicksort_line_plot.pdf')
 
     # Generating and save line plot for Insertion Sort
-    generate_plot(insertion_sort_df, xlabel='Input Size', ylabel='Running Time',
+    generate_plot(insertion_sort_df, xlabel='Input Size', ylabel='Running Time (sec)',
                   file_name='insertionsort_line_plot.pdf')
 
-    # Generating and save random line plot for all algorithms
-    generate_random_plot(['Merge Sort', 'Quick Sort', 'Insertion Sort'],
-                         [merge_sort_df, quicksort_df, insertion_sort_df],
-                         xlabel='Input Size',
-                         ylabel='Running Time',
-                         file_name='random_comparison_line_plot.pdf')
---
+++ plot_var.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import t
+
+# Read the results from pkl file and preprocess
+def read_preprocess_results(algorithm_name):
+    file_path = f'results/variance_{algorithm_name}.pkl'
+    df = pd.read_pickle(file_path)
+
+    # Calculating the mean execution time for each array size
+    mean_times = df.mean(axis=0)
+
+    # Calculating the standard error of the mean for each array size
+    std_err = df.std(axis=0) / np.sqrt(len(df))
+
+    # Calculating the degrees of freedom
+    degrees_of_freedom = len(df) - 1
+
+    # Calculating the confidence intervals using t-distribution
+    confidence = 0.95
+    confidence_intervals = std_err * t.interval(confidence, degrees_of_freedom)[1]
+
+    # Creating a new dataframe with columns for array size, mean time, and confidence intervals
+    data = pd.DataFrame({'Size': mean_times.index, 'Mean Time (sec)': mean_times.values, 'CI': confidence_intervals})
+
+    return data
+
+# Plot mean with the variance
+def plot_variance(preprocessed_dfs, algorithm_names):
+    plt.figure(figsize=(10, 6))
+    plt.rcParams.update({'font.size': 18})
+
+    # Plotting each algorithm's variance
+    for preprocessed_df, algorithm_name in zip(preprocessed_dfs, algorithm_names):
+        plt.errorbar(preprocessed_df['Size'], preprocessed_df['Mean Time (sec)'], yerr=preprocessed_df['CI'],
+                     fmt='-o', label=algorithm_name)
+        plt.fill_between(preprocessed_df['Size'], preprocessed_df['Mean Time (sec)'] - preprocessed_df['CI'],
+                         preprocessed_df['Mean Time (sec)'] + preprocessed_df['CI'], alpha=0.3)
+
+    plt.xlabel('Size')
+    plt.ylabel('Time')
+    plt.rcParams.update({'font.size': 18})
+    plt.grid(True)
+    plt.savefig('variance_plot.pdf')
+    plt.show()
+
+if __name__ == "__main__":
+    # Combining the benchmark results from different algorithms
+    merge_sort_df = read_preprocess_results('mergeSort')
+    quicksort_df = read_preprocess_results('quicksort')
+    insertion_sort_df = read_preprocess_results('insertionSort')
+
+    # Plot variance for each algorithm
+    plot_variance([merge_sort_df, insertion_sort_df, quicksort_df], ['Merge Sort', 'Insertion Sort', 'Quick Sort'])
